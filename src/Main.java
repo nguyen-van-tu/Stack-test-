0bbee97dd145113dba57a3057b57536d0c36d163
vantu <nguyenvantu2972002@gmail.com>
2020-12-23 10:35:14 +0700

    thuchanh

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    GenericStackClient genericStackClient = new GenericStackClient();
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+
+        System.out.println("\n2. Stack of Strings");
+
+    }
+}
